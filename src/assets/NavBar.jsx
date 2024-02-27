d99ef7316d9b95ad7d50b04c0e218a2246ea73b9
sarajac1 <sarajac@hotmail.com>
2024-02-27 12:48:15 +0100

    Started working on a navbar that routes between pages.

---
+++ src/assets/NavBar.jsx
@@ -0,0 +1,10 @@
+import { Link } from "react-router-dom"
+
+export default function NavBar() {
+  return (
+  <nav>
+      <Link to="/"></Link>
+      <Link to="/"></Link>
+  </nav >
+  )
+}
---
+++ src/assets/Router.jsx
@@ -0,0 +1,12 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+
+export default function Router() {
+  return (
+    <BrowserRouter>
+      <NavBar />
+      <Routes>
+        <Route />
+      </Routes>
+    </BrowserRouter>
+  )
+}
