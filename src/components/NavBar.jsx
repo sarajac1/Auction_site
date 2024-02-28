08629fc127b6c7b7760111155ace34389d166223
sarajac1 <sarajac@hotmail.com>
2024-02-28 14:02:26 +0100

    Centred nav bar successfully, added slogan.

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -3,11 +3,27 @@ import { Link } from "react-router-dom"
 
 export default function NavBar() {
   return (
-  <nav>
-      <Link to="/">Home</Link> |&nbsp;
-      <Link to="/About us">About us</Link> |&nbsp;
-      <Link to="/Jobs">Jobs</Link> |&nbsp;
-      <Link to="/Login">Login</Link>
-  </nav >
+
+
+
+    <div className="header">
+      <div className="nav-bar">
+        <nav className="nav-container">
+          <div className="left-and-middle-nav">
+            <div className="middle-nav-bar">
+              <Link to="/">Home</Link>
+              <Link to="/About us">About us</Link>
+              <Link to="/Jobs">Jobs</Link>
+            </div>
+          </div>
+          <div className="nav-bar-login">
+            <Link to="/Login">Login</Link>
+          </div>
+        </nav>
+        <div className="slogan">
+          <h1>Find your occult items today!</h1>
+        </div>
+      </div>
+    </div>
   )
 }
