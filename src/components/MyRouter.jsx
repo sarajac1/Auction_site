f050a2afdc18faf832f38b617f07dadc6a9419ae
ArsentyStreltsov <arsentystreltsov@gmail.com>
2024-02-29 15:13:39 +0100

    made bids page and listings page

--- src/components/MyRouter.jsx
+++ src/components/MyRouter.jsx
@@ -2,6 +2,10 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import NavBar from "../components/NavBar.jsx";
 import LoginPage from "../pages/loginPage.jsx";
 import HomePage from "../pages/HomePage.jsx";
+import ListingsPage from "../pages/ListingsPage";
+import BidsPage from "../pages/UserBidsPage";
+
+
 
 function MyRouter() {
   return (
@@ -10,6 +14,8 @@ function MyRouter() {
       <Routes>
         <Route path="/login-page" element={<LoginPage />} />
         <Route path="/" element={<HomePage />} />
+        <Route path="/listings" element={<ListingsPage />} />
+        <Route path="/bids" element={<BidsPage />} />
       </Routes>
     </BrowserRouter>
   );
---
+++ src/pages/ListingsPage.jsx
@@ -0,0 +1,11 @@
+
+import UserListings from "../components/UserListings";
+function ListingsPage() {
+  return (
+    <>
+      <UserListings userId="4" />
+    </>
+  );
+}
+
+export default ListingsPage;
