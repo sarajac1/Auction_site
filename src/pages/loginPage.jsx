7a348bad874a8081d5da902d20ccf4d45ffcd8dc
Rajesh <149164814+RJKN13@users.noreply.github.com>
2024-03-01 07:48:50 +0100

    Added registration form for new users

--- src/pages/loginPage.jsx
+++ src/pages/loginPage.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import ReactModal from 'react-modal';
 
 function LoginPage() {
   useEffect(() => {
@@ -16,7 +17,9 @@ function LoginPage() {
     username: '',
     password: ''
   });
+
   const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [showRegistrationForm, setRegistrationForm] = useState(false);
 
   const handleChange = (event) => {
     const { name, value } = event.target;
@@ -31,23 +34,37 @@ function LoginPage() {
 
     const existingUser = UsersList.find((obj) => obj.UserName === credentials.username);
 
-    if (existingUser != null) {      
+    if (existingUser != null) {
       localStorage.setItem('token', credentials.username);
       setIsLoggedIn(true);
     } else {
-      alert('User not found');
-      console.error('User not found! Do you want to register?');
+      if (confirm('User not found, Do you want to register?')) {
+        setRegistrationForm(true)  
+      }
+      else {
+        console.error('User not found!');
+      }      
     }
   };
+
 
   const handleLogout = () => {
     localStorage.removeItem('token');
     setIsLoggedIn(false);
   };
 
+  const handleCloseRegistrationForm = () => {
+    setRegistrationForm(false);
+  };
+
+  const handleRegistrationFormSubmit = () => { /*TO-DO: Add data to Users.json */
+    setRegistrationForm(false);
+  };
+
+
   return (
-    <div  className='user-info'>
-      {isLoggedIn ? (
+    <div className='user-info'>
+      {isLoggedIn  ? (
         <div>
           <p> Welcome {credentials.username}! &nbsp;
             <button onClick={handleLogout}>Logout</button> </p>
@@ -59,8 +76,31 @@ function LoginPage() {
           <button type="submit">Login</button>
         </form>
       )}
+
+      
+      {showRegistrationForm && (
+        <ReactModal isOpen={showRegistrationForm} contentLabel="Example Modal">
+        <div className="RegistrationForm">
+          <div className="RegistrationForm-content">
+            <span className="close" onClick={handleCloseRegistrationForm}>
+              &times;
+            </span>
+            <h2>Register User</h2>
+            
+            <form onSubmit={handleRegistrationFormSubmit}>
+              Username: <input type="text" name="UserName" placeholder="Username" value={credentials.username} onChange={handleChange} /> <br/>
+              Password: <input type="password" name="Password" placeholder="Password" value={credentials.password} onChange={handleChange} /> <br/>
+              Confirm Password: <input type="confirmpassword" name="Confirmpassword" placeholder="confirmPassword" value={credentials.password} onChange={handleChange} /> <br/>
+              Email: <input type="email" name="Email" placeholder="email" value={credentials.password} onChange={handleChange} /> <br/>
+              Address: <input type="address" name="Address" placeholder="Address" value={credentials.password} onChange={handleChange} /><br/>
+              <button type="submit">Register</button>
+            </form>
+          </div>
+          </div>
+          </ReactModal>
+      )}
     </div>
   );
-}
+}            
 
 export default LoginPage;
