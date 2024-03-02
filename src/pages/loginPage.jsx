517bc20c6d15d8ece306f989e019537adc8c19da
nighthail <natthagelmail@gmail.com>
2024-03-02 15:31:03 +0100

    Login-page now resembles mockup

--- src/pages/loginPage.jsx
+++ src/pages/loginPage.jsx
@@ -1,5 +1,5 @@
-import React, { useState, useEffect } from 'react';
-import ReactModal from 'react-modal';
+import React, { useState, useEffect } from "react";
+import ReactModal from "react-modal";
 
 function LoginPage() {
   useEffect(() => {
@@ -41,15 +41,13 @@ function LoginPage() {
       localStorage.setItem("token_id", existingUser.id);
       setIsLoggedIn(true);
     } else {
-      if (confirm('User not found, Do you want to register?')) {
-        setRegistrationForm(true)  
+      if (confirm("User not found, Do you want to register?")) {
+        setRegistrationForm(true);
+      } else {
+        console.error("User not found!");
       }
-      else {
-        console.error('User not found!');
-      }      
     }
   };
-
 
   const handleLogout = () => {
     localStorage.removeItem("token");
@@ -61,65 +59,122 @@ function LoginPage() {
     setRegistrationForm(false);
   };
 
-  const handleRegistrationFormSubmit = () => { /*TO-DO: Add data to Users.json */
+  const handleRegistrationFormSubmit = () => {
+    /*TO-DO: Add data to Users.json */
     setRegistrationForm(false);
   };
 
-
   return (
-    <div className='user-info'>
-      {isLoggedIn  ? (
-        <div>
-          <p>
-            {" "}
-            Welcome {credentials.username}! &nbsp;
-            <button onClick={handleLogout}>Logout</button>{" "}
-          </p>
-        </div>
-      ) : (
-        <form onSubmit={handleSubmit}>
-          <input
-            type="text"
-            name="username"
-            placeholder="Username"
-            value={credentials.username}
-            onChange={handleChange}
-          />
-          <input
-            type="password"
-            name="password"
-            placeholder="Password"
-            value={credentials.password}
-            onChange={handleChange}
-          />
-          <button type="submit">Login</button>
-        </form>
-      )}
+    <div className="container">
+      <div className="login-box">
+        <div className="user-info">
+          {isLoggedIn ? (
+            <div>
+              <p>
+                {" "}
+                Welcome {credentials.username}! &nbsp;
+                <button className="rounded-button-small" onClick={handleLogout}>
+                  Logout
+                </button>{" "}
+              </p>
+            </div>
+          ) : (
+            <>
+              <h1>Login</h1>
+              <form onSubmit={handleSubmit}>
+                <input
+                  type="text"
+                  name="username"
+                  placeholder="Username"
+                  value={credentials.username}
+                  onChange={handleChange}
+                />
+                <br />
+                <input
+                  type="password"
+                  name="password"
+                  placeholder="Password"
+                  value={credentials.password}
+                  onChange={handleChange}
+                />
+                <br />
+                <button className="rounded-button-small" type="submit">
+                  Login
+                </button>
+              </form>
+            </>
+          )}
 
-      
-      {showRegistrationForm && (
-        <ReactModal isOpen={showRegistrationForm} contentLabel="Example Modal">
-        <div className="RegistrationForm">
-          <div className="RegistrationForm-content">
-            <span className="close" onClick={handleCloseRegistrationForm}>
-              &times;
-            </span>
-            <h2>Register User</h2>
-            
-            <form onSubmit={handleRegistrationFormSubmit}>
-              Username: <input type="text" name="UserName" placeholder="Username" value={credentials.username} onChange={handleChange} /> <br/>
-              Password: <input type="password" name="Password" placeholder="Password" value={credentials.password} onChange={handleChange} /> <br/>
-              Confirm Password: <input type="confirmpassword" name="Confirmpassword" placeholder="confirmPassword" value={credentials.password} onChange={handleChange} /> <br/>
-              Email: <input type="email" name="Email" placeholder="email" value={credentials.password} onChange={handleChange} /> <br/>
-              Address: <input type="address" name="Address" placeholder="Address" value={credentials.password} onChange={handleChange} /><br/>
-              <button type="submit">Register</button>
-            </form>
-          </div>
-          </div>
-          </ReactModal>
-      )}
+          {showRegistrationForm && (
+            <ReactModal
+              isOpen={showRegistrationForm}
+              contentLabel="Example Modal"
+            >
+              <div className="RegistrationForm">
+                <div className="RegistrationForm-content">
+                  <span className="close" onClick={handleCloseRegistrationForm}>
+                    &times;
+                  </span>
+                  <h2>Register User</h2>
+
+                  <form onSubmit={handleRegistrationFormSubmit}>
+                    Username:{" "}
+                    <input
+                      type="text"
+                      name="UserName"
+                      placeholder="Username"
+                      value={credentials.username}
+                      onChange={handleChange}
+                    />{" "}
+                    <br />
+                    Password:{" "}
+                    <input
+                      type="password"
+                      name="Password"
+                      placeholder="Password"
+                      value={credentials.password}
+                      onChange={handleChange}
+                    />{" "}
+                    <br />
+                    Confirm Password:{" "}
+                    <input
+                      type="confirmpassword"
+                      name="Confirmpassword"
+                      placeholder="confirmPassword"
+                      value={credentials.password}
+                      onChange={handleChange}
+                    />{" "}
+                    <br />
+                    Email:{" "}
+                    <input
+                      type="email"
+                      name="Email"
+                      placeholder="email"
+                      value={credentials.password}
+                      onChange={handleChange}
+                    />{" "}
+                    <br />
+                    Address:{" "}
+                    <input
+                      type="address"
+                      name="Address"
+                      placeholder="Address"
+                      value={credentials.password}
+                      onChange={handleChange}
+                    />
+                    <br />
+                    <button className="rounded-button-small" type="submit">
+                      Register
+                    </button>
+                  </form>
+                </div>
+              </div>
+            </ReactModal>
+          )}
+        </div>
+      </div>
     </div>
   );
-}            
+}
 
 export default LoginPage;
