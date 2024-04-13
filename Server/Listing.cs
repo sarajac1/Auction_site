1674dc5767164455105b4cf42af8aa1a25be6804
Rajesh <149164814+RJKN13@users.noreply.github.com>
2024-04-13 16:33:29 +0200

    Fixed username display in LisitingsPage for Admin view

--- Server/Listing.cs
+++ Server/Listing.cs
@@ -5,6 +5,7 @@ public class Listing
     public int id { get; set; }
     public int sellerid { get; set; }
     public string? title { get; set; }
+    public string? sellername { get; set; }
     public string? description { get; set; }
     public string? image { get; set; }
     public DateTime startdate { get; set; }
@@ -23,51 +24,52 @@ public static class Listings
         decimal StartBid);
     public static IResult Post(PostData data, State state)
     {
-                 string query = "INSERT INTO listings (sellerid, title, description, image, startbid) VALUES (@SellerId, @Title, @Description, @Image, @StartBid)";
-            var result = MySqlHelper.ExecuteNonQuery(state.DB, query, [
-                new("@SellerId", data.SellerId),
+        string query = "INSERT INTO listings (sellerid, title, description, image, startbid) VALUES (@SellerId, @Title, @Description, @Image, @StartBid)";
+        var result = MySqlHelper.ExecuteNonQuery(state.DB, query, [
+            new("@SellerId", data.SellerId),
                 new("@Title", data.Title),
                 new("@Description", data.Description),
                 new("@Image", data.Image),
                 new("@StartBid", data.StartBid)
-            ]);
-            if (result == 1)
-            {
-                return TypedResults.Created();
-            }
-            else
-            {
-                return TypedResults.Problem();
-            }
-    
+        ]);
+        if (result == 1)
+        {
+            return TypedResults.Created();
+        }
+        else
+        {
+            return TypedResults.Problem();
+        }
+
     }
     public static List<Listing> GetAllListings(State state)
     {
         var listings = new List<Listing>();
-    
-           var reader= MySqlHelper.ExecuteReader(state.DB,"SELECT * FROM listings");
+
+        var reader = MySqlHelper.ExecuteReader(state.DB, "SELECT listings.*, users.username FROM listings LEFT JOIN users ON listings.sellerid = users.id;");
 
 
-            while (reader.Read())
+        while (reader.Read())
+        {
+            var listing = new Listing
             {
-                var listing = new Listing
-                {
-                    id = reader.GetInt32("id"),
-                    sellerid = reader.GetInt32("sellerid"),
-                    title = reader["title"] as string,
-                    description = reader["description"] as string,
-                    image = reader["image"] as string,
-                    startdate = reader.GetDateTime("startdate"),
-                    enddate = reader.GetDateTime("enddate"),
-                    startbid = reader.GetInt32("startbid")
-                };
-                listings.Add(listing);
-            }
-        
-      
+                id = reader.GetInt32("id"),
+                sellerid = reader.GetInt32("sellerid"),
+                title = reader["title"] as string,
+                description = reader["description"] as string,
+                image = reader["image"] as string,
+                startdate = reader.GetDateTime("startdate"),
+                enddate = reader.GetDateTime("enddate"),
+                startbid = reader.GetInt32("startbid"),
+                sellername = reader["username"] as string
+            };
+            listings.Add(listing);
+        }
+
+
         return listings;
     }
-    
+
 }
 
 
