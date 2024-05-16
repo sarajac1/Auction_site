8d551ec85e3ebbbec633b1e7b288a345360a52f1
ti3ktock <laura.tailor@gmail.com>
2024-05-16 10:58:08 +0200

    removed the js file and rewrote a bit the feature file, made new step. got it to add and update the balance but the test doesnt pinpoint that the update is done. still doing changes

--- cypress/specs/addBalanceInProfilePage.feature
+++ cypress/specs/addBalanceInProfilePage.feature
@@ -1,14 +1,8 @@
 Feature: Add balance in profile page
     Could able to add balance
     
-  Scenario: Login & View Profile page
-    Given I am in login page
-    When I have enter user credentials
-    When I click login button, I should be logged-in
-    Then I should see and click "profile" in the navbar to see the profilepage
-  
-
-  Scenario: Add balance in Profile page
-    When I enter "500" to update balance
-    When I click "addbalance" button
-    Then current balance should update
+  Scenario: Login, view balance page, and add balance
+    Given I log in with username "usifer" and password "123abc"
+    And that I am on "/profile" pagee
+    When I enter "500" in the "bid-input"
+    Then I click on the "addbalance" button the balance is updated
--- cypress/specs/step_definitions/addBalanceInProfilePage.js
+++ cypress/specs/step_definitions/addBalanceInProfilePage.js
@@ -1,38 +1,20 @@
-import { Given, When, Then } from "@badeball/cypress-cucumber-preprocessor";
+  import { Given, When, Then } from "@badeball/cypress-cucumber-preprocessor";
 
-Given('I am in login page', () => {
-  cy.visit('/login-page'); 
+  Given('that I am on {string} pagee', (pageUrl) => {
+    cy.visit(pageUrl);
+  });
+
+Given('that I am on {string}', (pageUrl) => {
+  cy.visit(pageUrl);
 });
 
-When('I have enter user credentials', () => {
-  cy.get('input[name="username"]').type('sophiemiller'); 
-  cy.get('input[name="password"]').type('sophie789'); 
+When('I enter {string} in the {string}', (value, field) => {
+  if (field === "bid-input") {
+    cy.get(`#${field}`).clear().type(value);
+  }
 });
 
-When('I click login button, I should be logged-in', () => {
-  cy.get('button[type="submit"]').click();
+Then('I click on the {string} button the balance is updated', (buttonId) => {
+  cy.get(`#${buttonId}`).click();
 });
-
-Then('I should see and click {string} in the navbar to see the profilepage', () => {
-  cy.get('[href="/profile"]').click();
-});
-
-When('I enter {string} to update balance', (balance) => {
-  cy.get('#bid-input').type(balance);
-});
-
-When('I click {string} button', (addbalance) => {
-  cy.get(`#${addbalance}`).click();
-});
-
-Then('current balance should update', () => {
-  cy.get('#currentBalance > .profile_info_different_collor');
-});
-
-
-
-
-
-
-
 
