a57c238ef506e524b7284131f21efcb849cd1364
ti3ktock <laura.tailor@gmail.com>
2024-05-16 12:49:35 +0200

    bidding feature is adjusted so that it adds 1 soul to the bid

--- cypress/specs/bidding.feature
+++ cypress/specs/bidding.feature
@@ -1,12 +1,11 @@
 Feature: Bidding
-    Check that i can make a bid
-    
-  Background:
-    Given I log in with username "usifer" and password "123abc"
-    And that I am on "/item/1" page
+    Check that i can make a bid   
   
   Scenario: bid
-    When I enter a "bidAmount" "625"
+    Given I log in with username "usifer" and password "123abc"
+    And that I am on "/item/1" page
+    Given I fetch the latest bid
+    When I enter the new bid amount
     And I click on the the "placeBidButton"
-    Then "highestBid" updates to the inputted "625"
+    Then "highestBid" updates to the new bid amount
 
--- cypress/specs/step_definitions/bidding.js
+++ cypress/specs/step_definitions/bidding.js
@@ -1,20 +1,31 @@
 import { Given, When, Then } from "@badeball/cypress-cucumber-preprocessor";
 
+let latestBid;
 
 Given('that I am on {string} page', (pageUrl) => {
   cy.visit(pageUrl);
 });
 
-When('I enter a {string} {string}', (field, value) => {
-  if (field === "bidAmount") {
-    cy.get('#bid-input').clear().type(value);
-  }
+Given('I fetch the latest bid', () => {
+  cy.get('#highestBid').invoke('text').then((bidText) => {
+    latestBid = parseInt(bidText, 10);
+    const newBid = latestBid + 1;
+    cy.wrap(newBid).as('newBidAmount');
+  });
+});
+
+When('I enter the new bid amount', () => {
+  cy.get('@newBidAmount').then((newBid) => {
+    cy.get('#bid-input').clear().type(newBid);
+  });
 });
 
 When('I click on the the {string}', (buttonId) => {
   cy.get(`#${buttonId} button`).click();
 });
 
-Then('{string} updates to the inputted {string}', (elementId, expectedValue) => {
-  cy.get(`#${elementId}`).should('have.text', `${expectedValue} Souls`);
+Then('{string} updates to the new bid amount', (elementId) => {
+  cy.get('@newBidAmount').then((newBid) => {
+    cy.get(`#${elementId}`).should('have.text', `${newBid} Souls`);
+  });
 });
